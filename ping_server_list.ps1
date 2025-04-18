82d1012970c2693c12c0565191ce546ebef15794
spardinas <59160259+spardinas@users.noreply.github.com>
2025-04-18 12:14:01 +0200

    Create ping_server_list.ps1

---
+++ ping_server_list.ps1
@@ -0,0 +1,15 @@
+# get a list of servers/computers to ping
+# save the status to a txt file for easy review
+
+
+$servers = Get-Content C:\path\to\serverlist.txt
+ForEach ($server in $servers) {
+ Try {
+  $IP = ((Test-Connection -ea stop -Count 1 -comp $server).IPV4Address).IPAddresstoString
+  "$server - UP - $($IP)" >> C:\path\to\serverliststatus.txt
+  } 
+ Catch {
+  Write-Warning "$_"
+  "$server - Down - $($_)" >> D:\Marejadilla\serverliststatus.txt
+  }
+ }
